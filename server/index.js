2501e067deb4c0eb1e2b43c60385e46abd0f98fb
pedromm65 <pedromm65@hotmail.com.br>
2022-02-07 21:38:40 -0300

    Finishing feature Add_route_get

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
+app.get("/codex", (req, res) => {
+    res.json({ message: "Hello codeX!" });
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
